3a8c333293fa71b4e33e265c6f2c8f5c1f959ffe
Ellery Buntel <buntel.ellery@gmail.com>
2023-11-29 15:11:01 -0500

    Added initial experiment, words.json, + readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fnirsTest
+
+A simple script for running a BCI experiment on words using PsychoPy for experimental setup, and PyLSL for coordinating data collection. 
---
+++ mainexperiment.py
@@ -0,0 +1,58 @@
+import json
+import random
+from psychopy import visual, core, event
+from pylsl import StreamInfo, StreamOutlet
+
+
+def get_keypress():
+    keys = event.getKeys()
+    if keys:
+        return keys[0]
+    else:
+        return None
+
+# Create a new LSL stream
+info = StreamInfo('WordMarkers', 'Markers', 1, 0, 'int32', 'wordstream')
+outlet = StreamOutlet(info)
+
+# Load words from a JSON file
+with open('words.json', 'r') as file:
+    data = json.load(file)
+    words = data["words"]
+
+# Randomize the order of the words
+random.shuffle(words)
+
+# Set up a PsychoPy window
+win = visual.Window(fullscr=True, color=(0, 0, 0))
+
+core.wait(2.5)
+
+# Main loop for displaying words and sending markers
+for word_info in words:
+    # Check for keypresses
+    keypress = get_keypress()
+
+    if keypress:
+        if keypress == "escape":
+            exit()
+        else:
+            continue
+
+    # Display the word
+    text = visual.TextStim(win, text=word_info["word"], color=(1, 1, 1))
+    text.draw()
+    win.flip()
+
+    # Send the index as a marker
+    outlet.push_sample([word_info["index"]])
+
+    # Keep the word on screen for 5 seconds
+    core.wait(3)
+
+    # Show a blank screen for 5 seconds
+    win.flip()
+    core.wait(6)
+
+# Close the window
+win.close()
